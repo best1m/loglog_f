8b57093bd06d374f294092c16d51cef374a283d2
best1m <kthiswell@gmail.com>
2019-01-13 16:08:06 +0900

    add haeder

---
+++ src/Components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../scss/header.scss'
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="ll-header">
+            <div>난 머리야</div>
+      </div>
+    );
+  }
+}
+
+export default Header;
