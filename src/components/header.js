cf2da848014384c61d4bdc2f1382ad3424649f44
kthiswell <kthiswell@rocode.co.kr>
2019-01-15 15:09:36 +0900

    테마 컬러 변경 및 로고 추가

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import 'scss/components/header.scss';
+import logo from 'images/logo.png';
 
 class Header extends Component {
   render() {
     return (
       <div className="ll-header">
+        <img className="ll-logo" src={logo}/>
         <div className="ll-menu">
           <Link to="/" className="ll-items">HOME</Link>
           <Link to="/about" className="ll-items">ABOUT</Link>
