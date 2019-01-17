33890ca6150e787244336b46dd03a0e1099b8dc5
kthiswell <kthiswell@rocode.co.kr>
2019-01-17 18:17:17 +0900

    DB서버랑 호환

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -4,15 +4,27 @@ import 'scss/pages/homePage.scss';
 
 class Home extends Component {
 
+  componentDidMount(){
+    fetch('http://localhost:4000/topic')
+    .then(res => res.json())
+    .then(res => {
+      this.setState({posts : res.data});
+      console.log(res.data);
+    })
+    .catch(err => console.error(err))
+    
+  }
+
   state = {
-    posts : [
-        { name : 'kim', id : 1, src : 'http://www.freejupiter.com/wp-content/uploads/2018/08/Superfine-Modern-Surrealism-Art-And-Painting-Ideas-feature.jpg'},
-        { name : 'lee', id: 2, src : 'https://cdn.uploadvr.com/wp-content/uploads/2016/05/sea-ship-whale.jpg'},
-        { name : 'park', id : 3, src : 'https://www.demilked.com/magazine/wp-content/uploads/2016/12/french-surrealism-illustrations-guy-billout-15.jpg'},
-        { name : 'choi' , id : 4, src : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi2OU7WDxka-d9qqINJh1B17XA9R2BXBWQ8nNjh965lzLBMyVxIA'},
-        { name : 'moon', id : 5, src : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrn6E21kd1XhFGcl2aqyiNPg4Xu1N5pk54OmoRmIPCC7xvefOV'},
-        { name : 'bang', id : 6, src : 'http://webneel.com/daily/sites/default/files/images/daily/11-2012/surreal-painting-vladimir-kush%20(23).jpg'},
-    ]
+    // posts : [
+    //     { name : 'kim', id : 1, src : 'http://www.freejupiter.com/wp-content/uploads/2018/08/Superfine-Modern-Surrealism-Art-And-Painting-Ideas-feature.jpg'},
+    //     { name : 'lee', id: 2, src : 'https://cdn.uploadvr.com/wp-content/uploads/2016/05/sea-ship-whale.jpg'},
+    //     { name : 'park', id : 3, src : 'https://www.demilked.com/magazine/wp-content/uploads/2016/12/french-surrealism-illustrations-guy-billout-15.jpg'},
+    //     { name : 'choi' , id : 4, src : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi2OU7WDxka-d9qqINJh1B17XA9R2BXBWQ8nNjh965lzLBMyVxIA'},
+    //     { name : 'moon', id : 5, src : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrn6E21kd1XhFGcl2aqyiNPg4Xu1N5pk54OmoRmIPCC7xvefOV'},
+    //     { name : 'bang', id : 6, src : 'http://webneel.com/daily/sites/default/files/images/daily/11-2012/surreal-painting-vladimir-kush%20(23).jpg'},
+    // ],
+    posts : []
 }
 
   render() {
